d89c303a65512df97995fc765c2db21a5a7e8bae
Albe1422 <128066343+Albe1422@users.noreply.github.com>
2023-05-29 17:59:48 +0200

    Add files via upload

---
+++ nuove.php
@@ -0,0 +1,33 @@
+<?php    //qeusta è la pagina dei nuovi album 
+?>
+<!doctype html>
+<html>
+    <head>
+    <link rel="stylesheet" href="page.css"/>
+    <script src="nuove.js" defer="true"></script>
+    
+        <title>
+          Music Store
+        </title>
+    </head>
+    <body>
+    
+    
+    
+    <nav>
+    <a href="home.php">Home</a>
+    <a href="nuove.php" class="nuove">Nuovi Album</a>
+    <a href="page.php">Cerca musica</a>
+    <a href="preferiti.php">Preferiti</a>
+    <a href="logout.php">Logout</a>
+  </nav>
+        <img class="copertina" src="music.png" />
+        
+        <p class="testo">Ultimi Usciti ♫</p>
+
+    <section id="album-view">
+   
+    </section>
+    
+    </body>
+</html>
---
+++ stampa.php
@@ -0,0 +1,19 @@
+<?php
+
+
+
+session_start();
+   $conn = mysqli_connect("localhost", "root", "", "musica") or die(mysqli_error($conn));
+   $nome =$_SESSION['username'];
+   $query = "SELECT a.autore, a.titolo, a.contenuto, a.img from articoli a join likes l where a.titolo=l.titolo and l.username='$nome'";
+  
+   $res = mysqli_query($conn, $query);
+   
+
+    $risultati= [];
+    while($row = mysqli_fetch_assoc($res)){
+      $risultati[]=$row;
+    }
+    echo json_encode($risultati);
+
+?>
